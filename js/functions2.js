4b9ccb116414d553be7e92d840af3c6f333aad7a
André Queiroz <andre_csq@hotmail.com>
2016-04-17 04:55:18 -0300

    adicionando mensagem final

--- js/functions2.js
+++ js/functions2.js
@@ -11,6 +11,10 @@ function GetURLParameter(sParam){
         }
     }
 }
+/*
+function appendToJSON(pergunta, resposta, email, telefone){
+	clientes.push({pergunta : pergunta, resposta: resposta, email: email, telefone: telefone});
+}*/
 
 $(document).ready(function() {
 
@@ -28,12 +32,30 @@ $(document).ready(function() {
 	/* carregando os nomes */
 	$("#lbl_p1").html(p1[p[0]].title + "<br>R$" + p1[p[0]].price);	
 	$("#lbl_p2").html(p2[p[1]].title + "<br>R$" + p2[p[1]].price);	
-	$("#lbl_p3").attr(p3[p[2]].title + "<br>R$" + p3[p[2]].price);
+	$("#lbl_p3").html(p3[p[2]].title + "<br>R$" + p3[p[2]].price);
 
 
 
 	$("#img_p1").click(function () {
-		console.log("mamma just killed a man");
+		swal({
+		  title: 'Ótima escolha!',
+		  html: '<p> Temos certeza que a sua mãe vai amar esse prato, ainda mais com esse precinho camarada que a gente conseguiu para você! Deixe o seu email aqui pra gente poder fazer a sua reserva e você poder tranquilo com a sua Mãe! Se quiser deixa o seu telefone também, assim fica mais fácil pra gente falar com você!</p><br>E-mail: <input class="inp" id="inp_email"><br>Telefone: <input class="inp" id="inp_tel">',
+		  showCancelButton: true,
+		  closeOnConfirm: false,
+		  allowEscapeKey: false,
+		  allowOutsideClick: false
+		}).then(function(isConfirm) {
+		  if (isConfirm) {
+
+		  	/*clientes.push({pergunta : 'p1', resposta: p[0], email: $('#inp_email').val(), telefone: $('#inp_tel').val()});*/
+		  	/*appendToJSON('p1', p[0], $('#inp_email').val(), $('#inp_tel').val());*/
+		    swal({
+		    	title: 'Obrigado por usar a DePara!',
+		    	html: 'Agora é só esperar o nosso contato!<br>Depara, gente feliz! 😁',
+		    	type: 'success'
+			})
+		  }
+		});
 	});
 
 
